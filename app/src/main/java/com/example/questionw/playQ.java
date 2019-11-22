48a13fcd933f81818d9a5f8fb7f95f406e999d36
AlexYudenkov <alexydenkov99@gmail.com>
2019-11-22 16:20:26 +0700

    Add right and wrong answers, random buttons

--- app/src/main/java/com/example/questionw/playQ.java
+++ app/src/main/java/com/example/questionw/playQ.java
@@ -10,11 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
+import java.util.Random;
 
 public class playQ extends AppCompatActivity implements View.OnClickListener {
     String StringQ;
@@ -22,8 +21,11 @@ public class playQ extends AppCompatActivity implements View.OnClickListener {
     Button button1;
     Button button2;
     Button button3;
-    final String FILENAME = "questionFile";
+    final String FILENAME = "questiontext.txt";
     final String LOG_TAG = "my_logs";
+    int i = 1;
+    String [] newq;
+    int ra;//правильный ответ
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,14 +39,19 @@ public class playQ extends AppCompatActivity implements View.OnClickListener {
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         //fwrite();
-        Question.setText(StringQ);
-         }
+
+    }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button1:
+                try {
+                    questions();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             case R.id.button2:
                 break;
@@ -66,19 +73,73 @@ public class playQ extends AppCompatActivity implements View.OnClickListener {
         }
     }
     //читает файл построчно
-    public void fread() throws IOException {BufferedReader reader = null;
+    public void fread(int o, String fname) throws IOException {BufferedReader reader = null;
 
             reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("questiontext.txt"), "UTF-8"));
-            // do reading, usually loop until end of file reading
+                    new InputStreamReader(getAssets().open(fname), "UTF-8"));
                 String mLine = "";
-                //process line
+                for (int i = 0;i<o;i++){
                 mLine = reader.readLine();
+                }
                 Log.d(LOG_TAG,mLine);
                 StringQ = mLine;
 
                 }
-    public void questions(){
-        
+    //количество строк в файле
+    public int linescounter(String fName) throws IOException {BufferedReader reader = null;
+
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open(fName), "UTF-8"));
+
+            // do reading, usually loop until end of file reading
+            String mLine = "";
+            //process line
+            int i = 0;
+            while ((mLine = reader.readLine()) != null) {
+                i++;
+            }
+            Log.e(LOG_TAG,"Столько сосчитали строк");
+            return i;
+
+    }
+
+    public void questions() throws IOException {
+          if(i<=linescounter(FILENAME)){//задаем вопросы,пока они не кончились
+          nextQuestion(i);
+          i++;} else {
+              //показать экран результатов
+          }
+    }
+
+    private void nextQuestion(int a) throws IOException {
+        fread(a,FILENAME);//прочитали строку из файла
+        newq = StringQ.split(" ");
+        Question.setText(newq[0]);//установили вопрос
+        int t = random();
+         switch (t){
+             case 1://первая кнопка правильная
+                 button1.setText(newq[1]);
+                 button2.setText(newq[2]);
+                 button3.setText(newq[3]);
+                 ra = 1;
+                 break;
+             case 2://вторая кнопка правильная
+                 button1.setText(newq[3]);
+                 button2.setText(newq[1]);
+                 button3.setText(newq[2]);
+                 ra = 2;
+                 break;
+             case 3://третья кнопка правильная
+                 button1.setText(newq[2]);
+                 button2.setText(newq[3]);
+                 button3.setText(newq[1]);
+                 ra = 3;
+                 break;
+         }
+    }
+    public int random (){//рандомное число от 1 до 3
+        Random Rand = new Random();
+        int b = Rand.nextInt(3);
+        return b;
     }
 }
